1164e0ef67e19e836f9c6100d30665d4b0f5ef8f
EungBug <eungb14@gmail.com>
2023-04-07 13:49:10 +0900

    장바구니 아이템 추가 및 삭제 구현중

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+// new Date()
+// createAttribute()
+// setAttribute()
+// appendChild() o
+// filter()
+// map()
+
 const inputEl = document.querySelector('.input-box input');
 const btnAddEl = document.querySelector('.input-box .material-icons');
 const listEl = document.querySelector('.list ul');
 
-btnAddEl.addEventListener('click', () => {
+const btnClear = document.querySelector('.delete-all');
+btnClear.addEventListener('click', clearAll);
+
+btnAddEl.addEventListener('click', addItem);
+
+// 아이템 추가
+function addItem() {
   const input = inputEl.value;
-  console.log(inputEl);
-  console.log(input);
   const liEl = document.createElement('li');
+
   if (input === '') {
     return;
   }
@@ -16,6 +28,29 @@ btnAddEl.addEventListener('click', () => {
     <div class="edit material-icons">border_color</div>
     <div class="delete material-icons">delete</div>
   `;
+  listEl.appendChild(liEl);
 
-  listEl.append(liEl);
-});
+  const btnDelete = liEl.querySelector('.delete');
+  btnDelete.addEventListener('click', function () {
+    deleteItem(liEl);
+  });
+
+  inputEl.value = null; // Input 초기화
+}
+
+// 아이템 삭제
+function deleteItem(el) {
+  listEl.removeChild(el);
+}
+
+// TODO : 아이템 수정
+
+// 전체 삭제
+function clearAll() {
+  const items = listEl.querySelectorAll('li');
+  if (items.length > 0) {
+    items.forEach(el => {
+      listEl.removeChild(el);
+    });
+  }
+}
